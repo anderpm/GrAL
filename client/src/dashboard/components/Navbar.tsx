8e6fbec13b85681965e33753aed7787fbc6caec3
anderpm <6pander6@gmail.com>
2024-09-03 20:39:36 +0200

    Improved css

--- client/src/dashboard/components/Navbar.tsx
+++ client/src/dashboard/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/dashboard/components/navbar.scss';
 
 interface NavbarProps {
@@ -8,14 +8,23 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ onMenuSelect }) => {
 	const menuItems = ["Overview", "Define Scope", "Explore Website", "Select Sample", "Audit Sample", "View Report"];
 
+	// Estado para mantener el ítem seleccionado
+	const [selectedItem, setSelectedItem] = useState<string | null>(null);
+
+	// Función para manejar el clic en un ítem
+	const handleItemClick = (item: string) => {
+		setSelectedItem(item); // Actualiza el ítem seleccionado
+		onMenuSelect(item);    // Llama a la función proporcionada por las props
+	};
+
 	return (
 		<nav className="navbar">
 			<ul className="navbar-menu">
 				{menuItems.map(item => (
 					<li
 						key={item}
-						className="navbar-item"
-						onClick={() => onMenuSelect(item)}
+						className={`navbar-item ${selectedItem === item ? 'selected' : ''}`}
+						onClick={() => handleItemClick(item)}
 					>
 						{item}
 					</li>
